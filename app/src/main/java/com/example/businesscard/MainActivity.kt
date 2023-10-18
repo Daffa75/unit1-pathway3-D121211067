8156c656fd0c7451693733d36fbd71b5ee0ac107
Daffa Yuslim <andidaffa75@gmail.com>
2023-10-18 22:19:46 +0800

    feat(CardHeader): add card header

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -3,14 +3,27 @@ package com.example.businesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.businesscard.ui.theme.BusinessCardTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +35,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
                 }
             }
         }
@@ -30,17 +42,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CardHeader (name: String, title: String, modifier: Modifier = Modifier){
+    val image = painterResource(R.drawable.profilephotoblue)
+
+    Column {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .size(200.dp)
+                .clip(shape = CircleShape)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+        Text(
+            text = name,
+            fontSize = 56.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+
+        )
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            color = Color.White,
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = false)
 @Composable
-fun GreetingPreview() {
+fun BusinessCardPreview() {
     BusinessCardTheme {
-        Greeting("Android")
+        CardHeader(name = "Daffa Yuslim", title = "Junior Programmer")
     }
 }
