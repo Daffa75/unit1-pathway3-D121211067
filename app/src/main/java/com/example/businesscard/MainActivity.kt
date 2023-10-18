fc3d68e8e7e6f1714976f83513e9d4c3926f46d9
Daffa Yuslim <andidaffa75@gmail.com>
2023-10-18 23:26:15 +0800

    feat(CardContent): add bgimage and card content

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -14,12 +14,24 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.businesscard.ui.theme.BusinessCardTheme
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -51,20 +63,20 @@ fun CardHeader (name: String, title: String, modifier: Modifier = Modifier){
             contentDescription = null,
             contentScale = ContentScale.Fit,
             modifier = Modifier
-                .size(200.dp)
+                .size(125.dp)
                 .clip(shape = CircleShape)
                 .align(alignment = Alignment.CenterHorizontally)
         )
         Text(
             text = name,
-            fontSize = 56.sp,
+            fontSize = 36.sp,
             fontWeight = FontWeight.Bold,
             color = Color.White,
 
         )
         Text(
             text = title,
-            fontSize = 24.sp,
+            fontSize = 18.sp,
             color = Color.White,
             modifier = Modifier
                 .align(alignment = Alignment.CenterHorizontally)
@@ -72,10 +84,89 @@ fun CardHeader (name: String, title: String, modifier: Modifier = Modifier){
     }
 }
 
-@Preview(showBackground = false)
+@Composable
+fun CardFooter (email: String, linkedin: String, instagram: String, modifier: Modifier = Modifier){
+    val emailIcon = painterResource(R.drawable.envelope_solid)
+    val instagramIcon = painterResource(R.drawable.instagram)
+    val linkedinIcon = painterResource(R.drawable.linkedin)
+    val logoModifier = Modifier
+        .size(24.dp)
+
+    Column (verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        Row (horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+            Image(
+                painter = emailIcon,
+                contentDescription = null,
+                modifier = logoModifier,
+                colorFilter = ColorFilter.tint(Color.White),
+            )
+            Text(
+                text = email,
+                fontSize = 16.sp,
+                color = Color.White
+            )
+        }
+        Row (horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+            Image(
+                painter = instagramIcon,
+                contentDescription = null,
+                modifier = logoModifier,
+                colorFilter = ColorFilter.tint(Color.White),
+            )
+            Text(
+                text = instagram,
+                fontSize = 16.sp,
+                color = Color.White
+            )
+        }
+        Row (horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+            Image(
+                painter = linkedinIcon,
+                contentDescription = null,
+                modifier = logoModifier,
+                colorFilter = ColorFilter.tint(Color.White),
+            )
+            Text(
+                text = linkedin,
+                fontSize = 16.sp,
+                color = Color.White
+            )
+        }
+    }
+}
+
+@Composable
+fun CardContent (name: String, title: String, email: String, instagram: String, linkedin: String, modifier: Modifier = Modifier) {
+    val bgImage = painterResource(R.drawable.darkgradient)
+
+    Box (modifier = Modifier.fillMaxSize()){
+        Image(
+            painter = bgImage,
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+        Column (modifier = Modifier
+            .padding(vertical = 64.dp)
+            .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column {
+                Spacer(modifier = Modifier.height(8.dp))
+                CardHeader(name = name, title = title)
+            }
+            CardFooter(email = email, linkedin = linkedin, instagram = instagram)
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
 @Composable
 fun BusinessCardPreview() {
     BusinessCardTheme {
-        CardHeader(name = "Daffa Yuslim", title = "Junior Programmer")
+//        CardHeader(name = "Daffa Yuslim", title = "Junior Programmer")
+//        CardFooter(email = "andidaffa75@gmail.com", linkedin = "daffayuslim", instagram = "@daffayuslim")
+    CardContent(name = "Daffa Yuslim", title = "Junior Programmer", email = "andidaffa75@gmail.com", instagram = "@daffayuslim", linkedin = "daffayuslim")
     }
 }
